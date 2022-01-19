44e9c6ee8ca79bb0518f515d6087b4a65eac9198
Juan Mayoral <jmayoral@telecentro.net.ar>
2022-01-19 13:45:25 -0300

    add

--- add-sd.sh
+++ add-sd.sh
@@ -4,6 +4,8 @@ HOST=$1
 OS=$2
 ALL=$3
 FILE=$(find /srv/sensor/nagios/vol/etc/local -iname $1.cfg)
+ID=$(docker ps |grep -i naemon |awk '{print $NF}')
+ALLSERVICES=$(grep -i service_description $FILE |grep -iv ping |awk '{print $2}' | paste -s -d, -)
 
 if [ $# -eq 0 ]
   then
@@ -20,8 +22,9 @@ else
 
 fi
 
-aplicar_config () {
-    echo "Copiando configuracion para $HOST a $FILE"
+
+LINUX (){
+        echo "Copiando configuracion para $HOST a $FILE"
     if  [ "$OS" == "LINUX" ] && [ -z $ALL ]; then
         echo "OS: $OS. Copiando configuracion para $HOST a $FILE"
 cat << EOF >> $FILE
@@ -35,21 +38,26 @@ define servicedependency {
 }
 EOF
     echo "Config aplicada"
-else
-     echo "OS: $OS. Copiando configuracion para $HOST a $FILE for services: $ALL"
+elif [ "$OS" == "LINUX" ] && [ ! -z $ALL ]; then
+        echo "OS: $OS. Copiando configuracion para $HOST a $FILE for services: $ALLSERVICES"
 cat << EOF >> $FILE
 define servicedependency {
     host_name                           $HOST
     service_description                 PING
     dependent_host_name                 $HOST
-    dependent_service_description       $ALL
+    dependent_service_description       $ALLSERVICES
     execution_failure_criteria          c
     notification_failure_criteria       c
 }
 EOF
-        echo "Config aplicada"
-elif [ "$OS" == "WIN" ] && [ -z $ALL ]; then
-    echo "OS: $OS. Copiando configuracion para $HOST a $FILE"
+echo "Config aplicada"
+fi
+}
+
+WIN () {
+            echo "Copiando configuracion para $HOST a $FILE"
+    if  [ "$OS" == "WIN" ] && [ -z $ALL ]; then
+        echo "OS: $OS. Copiando configuracion para $HOST a $FILE"
 cat << EOF >> $FILE
 define servicedependency {
     host_name                           $HOST
@@ -60,59 +68,71 @@ define servicedependency {
     notification_failure_criteria       c
 }
 EOF
-        echo "Config aplicada"
-else
-     echo "OS: $OS. Copiando configuracion para $HOST a $FILE for services: $ALL"
+    echo "Config aplicada"
+elif [ "$OS" == "WIN" ] && [ ! -z $ALL ]; then
+        echo "OS: $OS. Copiando configuracion para $HOST a $FILE for services: $ALLSERVICES"
 cat << EOF >> $FILE
 define servicedependency {
     host_name                           $HOST
     service_description                 PING
     dependent_host_name                 $HOST
-    dependent_service_description       $ALL
+    dependent_service_description       $ALLSERVICES
     execution_failure_criteria          c
     notification_failure_criteria       c
 }
 EOF
-        echo "Config aplicada"
-elif [ "$OS" == "VMWARE" ] && [ -z $ALL ]; then
-    echo "OS: $OS. Copiando configuracion para $HOST a $FILE"
-cat << EOF >> $FILE
-define servicedependency {
-    host_name                           $HOST
-    service_description                 PING
-    dependent_host_name                 $HOST
-    dependent_service_description       CPU,HARDWARE,IO-READ,IO-WRITE,MEMORIA,NET-NIC,SWAP,VMFS-ALL
-    execution_failure_criteria          c
-    notification_failure_criteria       c
-}
-EOF
-        echo "Config aplicada"
-else
-     echo "OS: $OS. Copiando configuracion para $HOST a $FILE for services: $ALL"
-cat << EOF >> $FILE
-define servicedependency {
-    host_name                           $HOST
-    service_description                 PING
-    dependent_host_name                 $HOST
-    dependent_service_description       $ALL
-    execution_failure_criteria          c
-    notification_failure_criteria       c
-}
-EOF
-        echo "Config aplicada"
+echo "Config aplicada"
 fi
-
 }
-    
+
+VMWARE () {
+                echo "Copiando configuracion para $HOST a $FILE"
+    if  [ "$OS" == "VMWARE" ]; then
+        echo "OS: $OS. Copiando configuracion para $HOST a $FILE for services: $ALLSERVICES"
+cat << EOF >> $FILE
+define servicedependency {
+    host_name                           $HOST
+    service_description                 PING
+    dependent_host_name                 $HOST
+    dependent_service_description       $ALLSERVICES
+    execution_failure_criteria          c
+    notification_failure_criteria       c
+}
+EOF
+echo "Config aplicada"
+fi
+}
+
+if [ -z "$ALL" ]
+then
 while true; do
     read -p "Aplicar configuracion:
              Hostname: $HOST 
              File: $FILE
              OS: $OS
+             Services: BASICOS 
              -> Aplicar configuracion? (Yyes/Nno): "  yn
     case $yn in
-        [Yy]* ) aplicar_config ; break;;
+        [Yy]* ) $OS ; break;;
         [Nn]* ) echo "No se realizo ningun cambio"; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+else
+
+while true; do
+    read -p "Aplicar configuracion:
+             Hostname: $HOST 
+             File: $FILE
+             OS: $OS
+             Services: $ALLSERVICES
+             -> Aplicar configuracion? (Yyes/Nno): "  yn
+    case $yn in
+        [Yy]* ) $OS ; break;;
+        [Nn]* ) echo "No se realizo ningun cambio"; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+fi
