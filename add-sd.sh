0bca2755fbb956cafc58a1ec77f452c2264c897f
Juan Mayoral <jmayoral@telecentro.net.ar>
2022-01-19 14:55:07 -0300

    cambios en el add-sd

--- add-sd.sh
+++ add-sd.sh
@@ -6,7 +6,7 @@ ALL=$3
 FILE=$(find /srv/sensor/nagios/vol/etc/local -iname $1.cfg)
 ID=$(docker ps |grep -i naemon |awk '{print $NF}')
 #ALLSERVICES=$(grep -i service_description $FILE |grep -iv ping |grep -v "#" |awk '{print $2}' | paste -s -d, -)
-ALLSERVICES=$(docker exec -i $ID /bin/bash -c "pynag list WHERE host_name=PBXROSARIO and object_type=service" |grep service |grep -v PING" |awk '{print $2}' | cut -d '/' -f2 | paste -s -d, -)
+ALLSERVICES=$(docker exec -i $ID /bin/bash -c "pynag list WHERE host_name=$HOST and object_type=service" |grep service |grep -v "PING" |awk '{print $2}' | cut -d '/' -f2 | paste -s -d,)
 
 if [ $# -eq 0 ]
   then
@@ -137,4 +137,3 @@ while true; do
 done
 
 fi
-
